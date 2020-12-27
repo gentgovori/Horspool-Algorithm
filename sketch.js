aa46606050fa78b25b22fe1949d8b0c037f62b52
Gent <gentgovori@gmail.com>
2020-12-27 13:46:20 +0100

    first commit

---
+++ sketch.js
@@ -0,0 +1,25 @@
+function setup()
+{
+console.log("Jepni tekstin :");
+var teksti = readline();
+console.log("Jepni patternin qe duhet te kerkojme ?")
+var pattern = readline();
+
+
+
+
+}
+
+var NO_OF_CHARS = 256;
+
+// function max(a,b)
+// {
+    //bone Math.max(a,b);
+// return (a>b)?a:b;
+// }
+
+function badCharHeuristic(str,size,badchar)
+{
+    var i;
+    
+}
