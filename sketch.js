6258eaefba6d627657477465da4aef96d8cec1e1
Gent <gentgovori@gmail.com>
2020-12-27 17:36:22 +0100

    initial commit

--- sketch.js
+++ sketch.js
@@ -1,25 +1,71 @@
 function setup()
 {
 console.log("Jepni tekstin :");
-var teksti = readline();
-console.log("Jepni patternin qe duhet te kerkojme ?")
-var pattern = readline();
+var pattern = prompt('Jepni pattern :');
+var patternArray = Array.from(pattern);
+
+var teksti = prompt('Jepni tekstin :');
+var tekstiArray = Array.from(teksti);
+var index = HorsPoolMatching(pattern,teksti);
+
+alert('Teksti u gjet ne indeksin: '+index);
 
 
 
 
 }
 
-var NO_OF_CHARS = 256;
 
-// function max(a,b)
-// {
-    //bone Math.max(a,b);
-// return (a>b)?a:b;
-// }
-
-function badCharHeuristic(str,size,badchar)
+function ShiftTable(pattern)
 {
-    var i;
-    
+
+    //var alphabet ="abcdefghijklmnopqrstuvwxyz".split('').join(',');
+    var alphabet =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    var Table={};
+
+    for(var i =0;i<alphabet.length;i++)
+    {
+        Table[alphabet[i]] = 0;
+    }
+
+
+    var length1= Object.keys(Table).length;
+    for(var i =0;i<=length1-1;i++)
+    {
+        Table[alphabet[i]]=pattern.length;
+    }
+    for(j=0;j<=pattern.length-2;j++)
+    {
+        Table[pattern[j]]=pattern.length-1-j;
+    }
+    return Table;
+}
+
+function HorsPoolMatching(P,T)
+{
+    var tabela = ShiftTable(P);
+
+    var i  = P.length-1;
+
+    while(i<=T.length-1)
+    {
+        k=0;
+        while(k<=P.length-1 && P[P.length-1-k]==T[i-k])
+        {
+            k=k+1;
+        }
+
+        if(k==P.length)
+        {
+            return i-P.length+1;
+        }
+        else
+        {
+            i=i+tabela[T[i]];
+        }
+        
+
+    }
+    return -1;
+
 }
